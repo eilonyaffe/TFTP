a6d80a09147cc1bb48fa8af8abb4ef367de3149a
Eilon Yaffe <93070667+eilonyaffe@users.noreply.github.com>
2024-03-05 15:42:43 +0200

    Update connectionsHolder.java

--- server/tftp/connectionsHolder.java
+++ server/tftp/connectionsHolder.java
@@ -3,6 +3,7 @@ package bgu.spl.net.impl.tftp;
 public class connectionsHolder {
     // public static TftpConnections<byte[]> activeConnections = new TftpConnections<byte[]>();
     public static TftpConnections<byte[]> activeConnections;
+    public int userID = 0;
 
     public static void start(){
         activeConnections =  new TftpConnections<byte[]>();
@@ -10,5 +11,9 @@ public class connectionsHolder {
 
     public static TftpConnections<byte[]> get(){ //TODO maybe delete later
         return activeConnections;
+    }
+
+    public static int getUniqueID(){
+        return activeConnections.client_connections.size();
     }
 }
