50f7da78a7d6b1f7c57aa4c7b0a50afbd766fea6
EliyaYa123 <157639716+EliyaYa123@users.noreply.github.com>
2024-03-05 13:32:46 +0200

    Add files via upload

--- server/TftpConnections.java
+++ server/TftpConnections.java
@@ -7,23 +7,28 @@ import bgu.spl.net.srv.Connections;
 
 public class TftpConnections<T> implements Connections<T> {
 
-    protected ConcurrentHashMap<Integer, ConnectionHandler<T>> client_connections = new ConcurrentHashMap<>();
+    protected ConcurrentHashMap<Integer, Pair<ConnectionHandler<T>, Boolean>> client_connections = new ConcurrentHashMap<>(); //neya changed
     
     @Override
     public boolean connect(int connectionId, ConnectionHandler<T> handler){ //eilon - kept generic, but will be blockingConnectionHandler
         //TODO implement
 
         if (!this.client_connections.containsKey(connectionId)){
-            this.client_connections.put(connectionId, handler);
+            this.client_connections.put(connectionId, new Pair<>(handler, true));
             return true;
         }
+        //this.client_connections.remove(connectionId); //currently removes user with equal id to another user which is already logged in - should remove after sending him error pkg maybe?
         return false; //client is already exists in the network
+    }
+
+    public void basicConnect(int connectionId, ConnectionHandler<T> handler){
+        this.client_connections.put(connectionId, new Pair<>(handler, false));
     }
 
     @Override
     public boolean send(int connectionId, T msg){
         //TODO implement
-        this.client_connections.get(connectionId).send(msg);
+        this.client_connections.get(connectionId).getFirst().send(msg);
         return false; //eilon - change?
     }
 
