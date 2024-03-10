08dc3fee2e285f2cb2b790d729cede4496f48f98
Eilon Yaffe <93070667+eilonyaffe@users.noreply.github.com>
2024-03-10 21:17:03 +0200

    Update TftpServer.java

--- server/tftp/TftpServer.java
+++ server/tftp/TftpServer.java
@@ -1,16 +1,5 @@
 package bgu.spl.net.impl.tftp;
-
-import java.util.function.Supplier;
-
-import bgu.spl.net.api.BidiMessagingProtocol;
-import bgu.spl.net.api.MessageEncoderDecoder;
-import bgu.spl.net.api.MessagingProtocol;
-import bgu.spl.net.impl.echo.EchoProtocol;
-import bgu.spl.net.impl.echo.LineMessageEncoderDecoder;
-import bgu.spl.net.srv.BaseServer;
-import bgu.spl.net.srv.BlockingConnectionHandler;
 import bgu.spl.net.srv.Server;
-import bgu.spl.net.impl.tftp.connectionsHolder;
 
 
 public class TftpServer {
@@ -18,8 +7,7 @@ public class TftpServer {
     public static void main(String[] args) {
 
         connectionsHolder.start(); //initializes the connections db. a static object.
-        
-        // you can use any server... 
+       
         Server.threadPerClient(
                 7777, //port
                 () -> new TftpProtocol(), //protocol factory
